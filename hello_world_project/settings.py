32eef68cd9ff3eb95c33dff5ff4a3f441fd915f8
rahulrk23 <89280701+rahulrk23@users.noreply.github.com>
2022-06-08 14:03:40 -0400

    Update settings.py

--- hello_world_project/settings.py
+++ hello_world_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-75k2dp^!=ffhz!(2dp)!_-rr^nvi%%!#m2kc7o!dnaornp&2*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
